27bc45a4ca6516bf0b8dd52074d7786fc6ed8e36
Vlad <167963642+Kipvi@users.noreply.github.com>
2024-05-27 12:02:08 +0300

    Add files via upload

---
+++ test 8.py
@@ -0,0 +1,7 @@
+# sum_ = 0
+# input_number = int(input("Введите число: "))
+# while input_number != 0:
+#     sum_ += input_number
+#     input_number = int(input("Введите число: "))
+# print("Ответ:", sum_)
+
