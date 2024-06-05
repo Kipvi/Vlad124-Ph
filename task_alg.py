20daa3b5c9d30407862f52eb63510ace4ddb7aec
Vlad <167963642+Kipvi@users.noreply.github.com>
2024-06-05 12:34:31 +0300

    Add files via upload

---
+++ task_alg.py
@@ -0,0 +1,21 @@
+# a, s, p = 1, 0, 0
+# while True:
+#     if a > 10:
+#         break
+#     a += 2
+#     p += a
+#     s += p
+# print(f'Переменная S: = {s}')
+
+# a, s, p = 1, 0, 0
+# while a <= 10:
+#         a += 2
+#         p += a
+#         s += p
+# print(f'Переменная S: = {s}')
+
+s = 1
+for n in range(6):
+    s *= n
+print(s)
+
